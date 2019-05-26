562218951efdacd801f81e06c2f65d753484250c
rjc17 <castro.ralph17@gmail.com>
2019-05-27 01:45:04 +0800

    Created Method for Delete Get and Post Action Method

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -78,7 +78,8 @@ namespace GraniteHouse.Areas.Admin.Controllers
 
 
         //GET Details Action Method
-        public async Task<IActionResult> Details(int? id) {
+        public async Task<IActionResult> Details(int? id)
+        {
 
             if (id == null)
             {
@@ -92,6 +93,37 @@ namespace GraniteHouse.Areas.Admin.Controllers
             }
 
             return View(productTypes);
+        }
+
+
+        //GET Delete Action Method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Delete Action Method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
     }
